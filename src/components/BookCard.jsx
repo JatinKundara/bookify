6d0d9d5a98ab7aaee20113039e61d77e0a7c8a53
Jatin Kundara <152637042+JatinKundara@users.noreply.github.com>
2024-10-27 18:29:21 +0530

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -16,20 +16,20 @@ function BookCard(props) {
     const navigate = useNavigate()
 
     return (
-        <li className='overflow-hidden'>
+        <li className='overflow-hidden w-[14rem]'>
             <a onClick={(e) => navigate(`/book-details/${props.id}`)} className="group overflow-hidden cursor-pointer">
                 <img
                     src={url}
                     alt="Book Cover"
-                    className="h-[250px] object-cover transition duration-500 group-hover:scale-105 sm:h-[350px]"
+                    className="object-cover transition duration-500 group-hover:scale-105"
                 />
 
                 <div className="relative bg-white pt-1">
-                    <h3 className="text-xl font-semibold text-black group-hover:underline group-hover:underline-offset-4">
+                    <h3 className="text-xl font-semibold text-black group-hover:underline group-hover:underline-offset-4 overflow-hidden whitespace-nowrap text-ellipsis max-w-full">
                         {/* The Kite Runner */}
                         {props?.bname}
                     </h3>
-
+ 
                     <p>
                         <span className="text-sm tracking-wider text-gray-900"> Author - {props?.authorName || 'Unknown'}
                         </span>
